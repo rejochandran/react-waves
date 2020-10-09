4097aa572dd3765ade36458ec0a69868656122a6
Rejo Chandran <rejo.chandran@stratforge.com>
2020-10-09 14:41:52 +0530

    Updates

---
+++ precache-manifest.a46b9d59e93795c3f60dafb27be7ff03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b123479b586d0074f38832ddf092755c",
+    "url": "/react-waves/index.html"
+  },
+  {
+    "revision": "b8da4ce7294c52f0cfd0",
+    "url": "/react-waves/static/css/main.05cfc9a3.chunk.css"
+  },
+  {
+    "revision": "45b53d3496c60f658483",
+    "url": "/react-waves/static/js/2.fdeaa324.chunk.js"
+  },
+  {
+    "revision": "af31d1618f3ecc973ec344d6fcb15101",
+    "url": "/react-waves/static/js/2.fdeaa324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8da4ce7294c52f0cfd0",
+    "url": "/react-waves/static/js/main.e669a108.chunk.js"
+  },
+  {
+    "revision": "3ccfd7e4226652ca5ffe",
+    "url": "/react-waves/static/js/runtime-main.0f1cdf48.js"
+  },
+  {
+    "revision": "5aa39e7744227522390b8a59fb5250c3",
+    "url": "/react-waves/static/media/africa.5aa39e77.mp3"
+  },
+  {
+    "revision": "86e9eec564de69771b98afa8940760c1",
+    "url": "/react-waves/static/media/demo.86e9eec5.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-waves/precache-manifest.07df041b076a7257eb1e146440b4df34.js"
+  "/react-waves/precache-manifest.a46b9d59e93795c3f60dafb27be7ff03.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-waves/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-waves/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
